f5d106792b2a115bc2ee9bf80fcf34eb5471096c
Son Bui <son@blueberrylabs.com>
2021-10-16 15:21:21 +0700

    remove .serverless_nextjs

--- .serverless_nextjs/assets/_next/static/IddJIQsIRNbcQdcqOB3JU/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fstatic-with-data","\u002Fworker\u002F[...slug]","\u002Fstudent\u002F[id]","\u002Fuser\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
